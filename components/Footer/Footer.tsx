9d1b6d6f8d4f95a3f36e168fc041b49997231f56
mitkimii@mitkimi.com <i@mitkimi.com>
2026-01-11 21:08:39 +0800

    remove outline for icons

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -9,12 +9,28 @@ export default function Footer() {
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-content">
-          <p className="copyright">
-            Copyright &copy; {currentYear} Kimi Tin, i.mitkimi.com
-          </p>
+          <div className="copyright">
+            <p>Copyright &copy; {currentYear} Kimi Tin, i.mitkimi.com</p>
+            <p className="beian">京ICP备15063748号-3</p>
+          </div>
           <div className="links">
             <a href="https://github.com/mitkimi" target="_blank" rel="noopener noreferrer" className="link">
-              <span>Github</span>
+              <img src="/assets/github.svg" alt="GitHub" className="link-icon" />
+            </a>
+            <a href="https://mitkimi.com" target="_blank" rel="noopener noreferrer" className="link">
+              <img src="/assets/mitkimi.svg" alt="Website" className="link-icon" />
+            </a>
+            <a href="https://www.xiaohongshu.com/user/profile/5f93f224000000000101f415" target="_blank" rel="noopener noreferrer" className="link">
+              <img src="/assets/redbook.svg" alt="RedBook" className="link-icon" />
+            </a>
+            <a href="https://www.douyin.com/user/MS4wLjABAAAABsgLtxWm5TObmVTb_0hupDEUpZkwOnoRADJr7C7KWFo" target="_blank" rel="noopener noreferrer" className="link">
+              <img src="/assets/tiktok.svg" alt="TikTok" className="link-icon" />
+            </a>
+            <a href="https://www.youtube.com/@mitkimi" target="_blank" rel="noopener noreferrer" className="link">
+              <img src="/assets/youtube.svg" alt="YouTube" className="link-icon" />
+            </a>
+            <a href="https://kimitinorganist.cn" target="_blank" rel="noopener noreferrer" className="link">
+              <img src="/assets/organ.svg" alt="Organ" className="link-icon" />
             </a>
           </div>
         </div>
